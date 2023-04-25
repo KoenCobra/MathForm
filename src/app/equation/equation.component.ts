6b5a9ee7a0dda9b10759bccdaa7cca3781a5e007
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-25 08:49:22 +0200

    Added custom validation

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-equation',
@@ -11,13 +11,21 @@ export class EquationComponent {
     a: new FormControl(this.randomNumber()),
     b: new FormControl(this.randomNumber()),
     answer: new FormControl('')
-  })
+  }, [
+    (form: AbstractControl) => {
+      const {a, b, answer} = form.value;
+      if (a + b === parseInt(answer)) {
+        return null
+      }
+      return {addition: true};
+    }
+  ])
 
-  get a(){
+  get a() {
     return this.mathForm.value.a;
   }
 
-  get b(){
+  get b() {
     return this.mathForm.value.b;
   }
 
